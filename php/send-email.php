74cb2b30f74ec0a36d15a88f2f529ded93e8bc93
Filip Holesz <projekce@fvsol.cz>
2026-01-04 16:57:43 +0100

    Aktualizace všech souborů

--- php/send-email.php
+++ php/send-email.php
@@ -1,7 +1,7 @@
 <?php
 
 // Replace this with your own email address
-$to = 'joefrey.mahusay@gmail.com';
+$to = 'projekce@fvsol.cz';
 
 function url(){
   return sprintf(
@@ -41,7 +41,7 @@ if($_POST) {
    $mail = mail($to, $subject, $message, $headers);
 
 	if ($mail) { echo "OK"; }
-   else { echo "Something went wrong. Please try again."; }
+   else { echo "Zkuste to prosím znovu"; }
 
 }
 
